509be254d21bf4568fc93601e64f9a4c77517429
ahmed-saifulla <ahsaifulla@gmail.com>
2020-11-22 00:09:17 +0530

    tracking input value props, useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import './App.css';
-import Form from './components/Form'
+import React, { useState } from "react";
+import "./App.css";
+
+import Form from "./components/Form";
+import TodoList from "./components/TodoList.js";
+
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
       <header>
         <h1>Dev's Todo App</h1>
       </header>
-      <Form />
+      <Form setInputText={setInputText} />
+      <TodoList/>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,23 @@
 import React from "react";
 
-const Form = () => {
+const Form = ({ setInputText }) => {
+  const inputTextHandler = (e) => {
+    console.log(e.target.value);
+    setInputText(e.target.value)
+  };
+
   return (
     <form className="Form">
-      <input type="text" className="todo-input" />
+      <input type="text" className="todo-input" onChange={inputTextHandler} />
       <button type="submit" className="todo-add-btn">
         Add
       </button>
       <div className="select">
-          <select name="todos" className="todo-filter">
-              <option value="all">All</option>
-              <option value="completed">Completed</option>
-              <option value="uncompleted">Uncompleted</option>
-          </select>
+        <select name="todos" className="todo-filter">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
       </div>
     </form>
   );
